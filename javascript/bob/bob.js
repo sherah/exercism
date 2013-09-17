192fe2bcaf2ad8320f220bce847d8050a7c28cd0
sherah <sherah@sherahsmith.com>
2013-09-17 09:37:01 -0700

    iteration 2 javascript/bob

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,28 +1,43 @@
 var Bob = function(){
 
+  "use strict";
 
-  this.hey = function(conversationStarter){
-    var response;
+  this.hey = function(input){
 
-    if(!conversationStarter || conversationStarter.match(/^\s+$/)){
-      response = "Fine. Be that way!";
-    } else if(conversationStarter === conversationStarter.toUpperCase()){
-      response = 'Woah, chill out!';
-    } else if(conversationStarter.charAt(conversationStarter.length-1) === '?'){
-      response = 'Sure.';
-    } else {
-      response = "Whatever.";
-    }
+    var conversationStarter = new ConversationStarter(input);
 
-    return response;
+    if( conversationStarter.isSayingNothing() ){ return "Fine. Be that way!"; }
+    if( conversationStarter.isShouting() ){ return 'Woah, chill out!'; }
+    if( conversationStarter.isAskingQuestion() ){ return 'Sure.'; }
+    
+    return "Whatever.";
     
   };
 
 
-
 };
 
+var ConversationStarter = function(input){
 
+  this.isSayingNothing = function(){
+
+    if(!input || input.match(/^\s+$/)) { return true; }
+
+  };
+
+  this.isShouting = function(){
+
+    if(input === input.toUpperCase()) { return true; }
+
+  };
+
+  this.isAskingQuestion = function(){
+
+    if(input.charAt(input.length-1) === '?') { return true; }
+
+  };
+
+};
 
 
 module.exports = Bob;
